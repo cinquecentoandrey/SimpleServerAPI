540266bd1577664a58833ab05f71b9991ebe8bc6
Andrey <chehonte16@gmail.com>
2023-09-13 22:52:04 +0300

    Initial commit

---
+++ src/main/java/com/cinquecento/simpleserverapi/SimpleServerApiApplication.java
@@ -0,0 +1,13 @@
+package com.cinquecento.simpleserverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleServerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleServerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cinquecento/simpleserverapi/SimpleServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinquecento.simpleserverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleServerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
