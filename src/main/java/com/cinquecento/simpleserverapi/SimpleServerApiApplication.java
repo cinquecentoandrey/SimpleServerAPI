f011a6bb375215d85cb0e7f47bf45a74d1c13aa0
Andrey <chehonte16@gmail.com>
2023-09-15 21:15:07 +0300

    Added ModelMapper bean

--- src/main/java/com/cinquecento/simpleserverapi/SimpleServerApiApplication.java
+++ src/main/java/com/cinquecento/simpleserverapi/SimpleServerApiApplication.java
@@ -1,7 +1,9 @@
 package com.cinquecento.simpleserverapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SimpleServerApiApplication {
@@ -10,4 +12,8 @@ public class SimpleServerApiApplication {
 		SpringApplication.run(SimpleServerApiApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
