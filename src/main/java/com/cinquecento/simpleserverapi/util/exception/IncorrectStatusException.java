a791e87307941b548fa14fcda549a3c03662d45a
Andrey <chehonte16@gmail.com>
2023-09-15 21:14:35 +0300

    Added exceptions used in UserController

---
+++ src/main/java/com/cinquecento/simpleserverapi/util/exception/IncorrectStatusException.java
@@ -0,0 +1,7 @@
+package com.cinquecento.simpleserverapi.util.exception;
+
+public class IncorrectStatusException extends RuntimeException {
+    public IncorrectStatusException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/cinquecento/simpleserverapi/util/exception/UserNotCreatedException.java
@@ -0,0 +1,7 @@
+package com.cinquecento.simpleserverapi.util.exception;
+
+public class UserNotCreatedException extends RuntimeException {
+    public UserNotCreatedException(String message) {
+        super(message);
+    }
+}
