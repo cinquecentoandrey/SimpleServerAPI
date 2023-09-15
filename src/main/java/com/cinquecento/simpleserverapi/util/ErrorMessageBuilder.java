9cfea3fa709a4e36a20921f55f0410da2d78cfc2
Andrey <chehonte16@gmail.com>
2023-09-15 21:14:51 +0300

    Added util ErrorMessageBuilder

---
+++ src/main/java/com/cinquecento/simpleserverapi/util/ErrorMessageBuilder.java
@@ -0,0 +1,21 @@
+package com.cinquecento.simpleserverapi.util;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+
+@Component
+public class ErrorMessageBuilder {
+
+    public ErrorMessageBuilder() {}
+
+    public String message(BindingResult bindingResult) {
+        StringBuilder errorMessage = new StringBuilder();
+
+        bindingResult.getFieldErrors().forEach(e -> errorMessage.append(e.getField())
+                .append(" - ")
+                .append(e.getDefaultMessage())
+                .append("; "));
+
+        return errorMessage.toString();
+    }
+}
